bcdd37afd35e9ee7fb6ec34aa3dae13eea47d21d
esehigh <esehigh@gmail.com>
2019-11-26 10:36:50 +0100

    feature(aboutus-page):about us page create an aboutus page create main.css create index.html file create script.js file [finishes]

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//toggle navbar function
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
